35c99ce25cedb8d0f1f5b7647f321a96cc861253
Nazar Bodan <nazar.bodan@indeema.com>
2021-09-28 11:39:12 +0300

    format code style

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-var str = prompt("Введіть будь-яке слово")
+var str = prompt('Введіть будь-яке слово');
 
-function change_register(str){
-var newstr = ""
-  for(var i = 0; i < str.length; i++){
-     if (str.charAt(i) === str.charAt(i).toLowerCase()){
-        newstr += str.charAt(i).toUpperCase();
-     }
-     else {
-       newstr += str.charAt(i).toLowerCase()
-     }
+function change_register(str) {
+  var newstr = '';
+  for (var i = 0; i < str.length; i++) {
+    if (str.charAt(i) === str.charAt(i).toLowerCase()) {
+      newstr += str.charAt(i).toUpperCase();
+    } else {
+      newstr += str.charAt(i).toLowerCase();
+    }
   }
   return newstr;
 }
@@ -16,15 +15,14 @@ alert(change_register(str));
 
 //скрипт (код, що призведе до краху вкладки)
 //1
-let a = 0
-while(a<5){
-  a +=0.001
-  if(a>4.5 && a<4.55) 
-  alert(a)
+let a = 0;
+while (a < 5) {
+  a += 0.001;
+  if (a > 4.5 && a < 4.55) alert(a);
 }
 //2
-let a =5;
-while(a>2){
-a++;
-alert("?");
+let a = 5;
+while (a > 2) {
+  a++;
+  alert('?');
 }
